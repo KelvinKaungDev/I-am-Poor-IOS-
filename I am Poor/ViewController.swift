578782e65664e85ca71a46778fe1dd87fb30f717
KelvinKaungDev <u6511753@au.edu>
2022-12-29 15:55:09 +0700

    I am poor

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Kaung Htet OO on 12/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
